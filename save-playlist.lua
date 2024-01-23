66d4803cbef1306b1b15766cc91fb12693d250c2
Youxikong <tonglibg@gmail.com>
2024-01-23 21:03:25 +0800

    Add support for mpv-menu-plugin

--- save-playlist.lua
+++ save-playlist.lua
@@ -4,32 +4,58 @@ local playlist_savepath = mp.get_property('working-directory')
 local utils = require("mp.utils")
 local msg = require("mp.msg")
 
-local filename = nil
+local have_menu = false
 
---saves the current playlist into a m3u file
-function save_playlist()
-  local length = mp.get_property_number('playlist-count', 0)
-	if length == 0 then return end
-  local savepath = utils.join_path(playlist_savepath, os.time().."-size_"..length.."-playlist.m3u")
-  local file, err = io.open(savepath, "w")
-  if not file then
-	msg.error("Error in creating playlist file, check permissions and paths: "..(err or ""))
-  else
-	local i=0
-	while i < length do
-	  local pwd = mp.get_property("working-directory")
-	  local filename = mp.get_property('playlist/'..i..'/filename')
-	  local fullpath = filename
-	  if not filename:match("^%a%a+:%/%/") then
-		fullpath = utils.join_path(pwd, filename)
-	  end
-	  file:write(fullpath, "\n")
-	  i=i+1
+-- saves the current playlist into a m3u file
+function save_playlist(savepath)
+	local length = mp.get_property_number('playlist-count', 0)
+	local file, err = io.open(savepath, "w")
+	if not file then
+		msg.error("Error in creating playlist file, check permissions and paths: " .. (err or ""))
+	else
+		local i = 0
+		while i < length do
+			local pwd = mp.get_property("working-directory")
+			local filename = mp.get_property('playlist/' .. i .. '/filename')
+			local fullpath = filename
+			if not filename:match("^%a%a+:%/%/") then
+				fullpath = utils.join_path(pwd, filename)
+			end
+			file:write(fullpath, "\n")
+			i = i + 1
+		end
+		msg.info("Playlist written to: " .. savepath)
+		mp.osd_message("Playlist written to: " .. savepath)
+		file:close()
 	end
-	msg.info("Playlist written to: "..savepath)
-	mp.osd_message("Playlist written to: "..savepath)
-	file:close()
-  end
 end
 
-mp.add_key_binding("alt+s","save-playlist", save_playlist)
+-- detect menu.dll and register script message
+mp.register_script_message('menu-ready', function()
+	have_menu = true
+	mp.register_script_message('dialog-save-reply', function(savepath)
+		save_playlist(savepath)
+	end)
+end)
+
+mp.register_script_message('save-playlist', function()
+	local length = mp.get_property_number('playlist-count', 0)
+	if length == 0 then 
+		mp.osd_message("Empty Playlist")
+		return 
+	end
+
+	local filename = os.time() .. "-size_" .. length .. "-playlist"
+
+	if have_menu then
+		mp.set_property_native('user-data/menu/dialog/filters', {
+			{ name = 'Playlist Files',  spec = '*.m3u;*.m3u8;*.pls;*.cue' },
+			{ name = 'All Files (*.*)', spec = '*.*' },
+		})
+		mp.set_property_native('user-data/menu/dialog/default-name', filename)
+		mp.commandv('script-message-to', 'menu', 'dialog/save', mp.get_script_name())
+	else
+		local savepath = utils.join_path(playlist_savepath, filename .. '.m3u')
+		save_playlist(savepath)
+	end
+end)
